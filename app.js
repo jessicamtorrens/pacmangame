9f6ddc3ba9d8927dc11e3e841e9393b8013c2fa5
jessica_torr11@hotmail.com <jessica_torr11@hotmail.com>
2021-05-12 22:51:29 -0430

    Update app.js
    writing function so pacmancan eat dots

--- app.js
+++ app.js
@@ -109,6 +109,14 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   document.addEventListener('keyup', movePacman)
   
-  
+  //what happens when Pac-man eats a pacdot
+
+  function pacDotEaten() {
+    if(squares[pacmanCurrentIndex].classList.contains('pac-dot')) {
+      score++
+      scoreDisplay.innerHTML = score
+      squares[pacmanCurrentIndex].classList.remove('pac-dot')
+    }
+  }
   
 })
